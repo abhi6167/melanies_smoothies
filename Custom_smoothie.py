04477da4ef7a99586fcd16b4b5e9ae690ba05af7
abhi6167 <75129730+abhi6167@users.noreply.github.com>
2025-10-04 00:39:34 +0530

    Update Custom_smoothie.py

--- Custom_smoothie.py
+++ Custom_smoothie.py
@@ -15,7 +15,7 @@ st.write(f"Name of your smoothie will be: {name_on_order}")
 cnx = st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col("SEARCH_ON"))
-st.my_dataframe(data=my_dataframe, use_container_width=True)
+st.dataframe(data=my_dataframe, use_container_width=True)
 # st.stop()
 ingredients_list = st.multiselect(
     'choose upto 5 ingredients',
